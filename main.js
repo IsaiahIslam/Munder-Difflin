a99a53ece102097e6ddefc271ad96841bb2fd518
Isaiah Islam <IIslam@nyc.yearup.org>
2018-11-11 23:35:09 -0500

    still need practice working JavaScript

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-document.getElementsByClassName("javaS").onmouseover = function() {mouseOver()};
-document.getElementsByClassName("javaS").onmouseout = function() {mouseOut()};
+let button1 = document.getElementById('button1');
+let button2 = document.getElementById('button2');
+let button3 = document.getElementById('button3');
+let button4 = document.getElementById('button4');
 
-function mouseOver() {
-    document.getElementsByClassName("javaS").style.color = "#800000";
+button1.addEventListener("mouseover", colorChangeIn);
+button2.addEventListener("mouseover", colorChangeIn);
+button3.addEventListener("mouseover", colorChangeIn);
+button4.addEventListener("mouseover", colorChangeIn);
+
+button1.addEventListener("mouseout", colorChangeOut);
+button2.addEventListener("mouseout", colorChangeOut);
+button3.addEventListener("mouseout", colorChangeOut);
+button4.addEventListener("mouseout", colorChangeOut);
+
+function colorChangeIn(){
+  this.style.background = 'aqua',
+  this.style.color = 'black'
 }
 
-function mouseOut() {
-    document.getElementsByClassName("javaS").style.color = "black";
+function colorChangeOut(){
+  this.style.background = 'black',
+  this.style.color = 'white'
 }
