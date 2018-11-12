dd1d1b8b9703477da7580a936179a9640cf0e48c
Isaiah Islam <IIslam@nyc.yearup.org>
2018-11-12 00:01:43 -0500

    a little extra

--- main.js
+++ main.js
@@ -2,16 +2,19 @@ let button1 = document.getElementById('button1');
 let button2 = document.getElementById('button2');
 let button3 = document.getElementById('button3');
 let button4 = document.getElementById('button4');
+let button5 = document.getElementById('button5');
 
 button1.addEventListener("mouseover", colorChangeIn);
 button2.addEventListener("mouseover", colorChangeIn);
 button3.addEventListener("mouseover", colorChangeIn);
 button4.addEventListener("mouseover", colorChangeIn);
+button5.addEventListener("mouseover", colorChangeIn);
 
 button1.addEventListener("mouseout", colorChangeOut);
 button2.addEventListener("mouseout", colorChangeOut);
 button3.addEventListener("mouseout", colorChangeOut);
 button4.addEventListener("mouseout", colorChangeOut);
+button5.addEventListener("mouseout", colorChangeOut);
 
 function colorChangeIn(){
   this.style.background = 'aqua',
@@ -22,3 +25,7 @@ function colorChangeOut(){
   this.style.background = 'black',
   this.style.color = 'white'
 }
+
+button5.addEventListener('click', function(){
+  alert('We are currently not taking anymore members at this time.');
+});
